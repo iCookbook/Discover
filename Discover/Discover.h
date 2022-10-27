1faeec82bb13c47498bc28c146219b68ca83992a
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-10-27 19:33:17 +0300

    feat: complete module

---
+++ Discover/Discover.h
@@ -0,0 +1,18 @@
+//
+//  Discover.h
+//  Discover
+//
+//  Created by Егор Бадмаев on 27.10.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Discover.
+FOUNDATION_EXPORT double DiscoverVersionNumber;
+
+//! Project version string for Discover.
+FOUNDATION_EXPORT const unsigned char DiscoverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Discover/PublicHeader.h>
+
+
